cbc4b60a5b2f7d931242c1b000744289840298ec
Fangxi Huang <fangxi.huang@think-way.net>
2023-02-14 02:28:38 +0000

    Initial Commmit

---
+++ app/risks/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'risk/test/integration/FirstJourney',
+		'risk/test/integration/pages/RisksList',
+		'risk/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('risk') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
